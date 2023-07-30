b0fcda22d719bd2dc61b96e9e9badcbd9423138c
ricardo mejias <panswer.rjmm@gmail.com>
2023-07-30 11:31:53 -0400

    section 3 - Operadores logicos

--- section_3/HolaMundo.py
+++ section_3/HolaMundo.py
@@ -1,18 +1,11 @@
-# Operadores relacionales
-# r = 100 == 100
-# print(r)
+# Operadores logicos
 
-# r = (100 != 100)
-# print(r)
+a = 30
+b = 40
+c = 50
 
-# r = 50 < 100
-# print(r)
-
-# r = 100 <= 100
-# print(r)
-
-# r = 200 > 100
-# print(r)
-
-r = 100 >= 100
+# r = (a < b) and (b < c)
+# r = (a > b) and (b < c)
+# r = (a > b) or (b < c)
+r = not((a > b) or (b < c))
 print(r)
