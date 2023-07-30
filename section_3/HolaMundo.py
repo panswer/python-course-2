ddf75d1788771631eb157c59701ce8b48f18fae3
ricardo mejias <panswer.rjmm@gmail.com>
2023-07-30 11:40:39 -0400

    section 3 - Salida de datos

--- section_3/HolaMundo.py
+++ section_3/HolaMundo.py
@@ -1,9 +1,6 @@
-# Operadores de asignacion
-c = 0
-c += 10
-c -= 5
-c *= 3
-c /= 5
-c **= 3
-c %= 3
-print(c)
+# Salida de datos
+
+app = "flutter"
+proyecto = "ComFlu"
+
+print(f"Se hara en {app} se llamara {proyecto}")
