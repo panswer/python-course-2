6be04d619a84c0e45538d04fa26aa5a0ab44319a
ricardo mejias <panswer.rjmm@gmail.com>
2023-07-30 14:56:06 -0400

    section 3 - Ejercicio 1

---
+++ section_3/ejercicios/ejercicio1.py
@@ -0,0 +1,9 @@
+# Ejercicio 1
+
+a = float(input("Variable a: "))
+b = float(input("Variable b: "))
+c = float(input("Variable c: "))
+
+r = ((c + 5) * ((b ** 2) - 3 * a * c) * ( a ** 2))/(4 * a)
+
+print(f"El resultado es: {r}")
