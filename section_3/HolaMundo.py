1e8bfa58b9172f716c3757dd7186a44fa49df02a
ricardo mejias <panswer.rjmm@gmail.com>
2023-07-30 11:26:48 -0400

    section 3 - Operadores aritmeticos

--- section_3/HolaMundo.py
+++ section_3/HolaMundo.py
@@ -1,38 +1,18 @@
-# n1 = 3
-# n2 = 5
-# r = n1 + n2
+# Operadores relacionales
+# r = 100 == 100
 # print(r)
 
-# n1 = 6
-# n2 = 4
-# r = n1 - n2
+# r = (100 != 100)
 # print(r)
 
-# n1 = 3
-# n2 = 9
-# r = n1 * n2
+# r = 50 < 100
 # print(r)
 
-# n1 = 2
-# n2 = 3
-# r = n1 ** n2
+# r = 100 <= 100
 # print(r)
 
-# n1 = 7
-# n2 = 2
-# r = n1 / n2
+# r = 200 > 100
 # print(r)
 
-# n1 = 7
-# n2 = 2
-# r = n1 // n2
-# print(r)
-
-# n1 = 9
-# n2 = 3
-# r = n1 % n2
-# print(r)
-
-r: str = ( 5 + ( 9 / 3)) ** 2
+r = 100 >= 100
 print(r)
-print(type(r))
