d7cb68d9477f1e8147d5434459a029cfbc61b3d5
ricardo mejias <panswer.rjmm@gmail.com>
2023-07-30 08:58:04 -0400

    section 3 - Tipos de datos

--- section_3/HolaMundo.py
+++ section_3/HolaMundo.py
@@ -1,2 +1,26 @@
-print("Hola Mundo")
-print("Soy Ricardo")
+# print("Hola Mundo")
+# print("Soy Ricardo")
+
+# numero = 50.6
+# print(numero)
+# print(type(numero))
+
+# palabra = "Hola de nuevo"
+# print(palabra)
+# print(type(palabra))
+
+# dato = False
+# print(dato)
+# print(type(dato))
+
+# num1 = 120
+# num2 = 12.8
+# sum = (num1 + num2 * 5) / 12
+# print("Resultado:", sum)
+
+dato = 60
+print(dato)
+dato = "cambio de valor"
+print(dato)
+dato = 12.8
+print(dato)
