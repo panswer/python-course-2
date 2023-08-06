01a7e5748026c7db9645656c6994d504c50210b7
ricardo mejias <panswer.rjmm@gmail.com>
2023-08-06 11:11:18 -0400

    section 4 - Condicional if

---
+++ section_4/if.py
@@ -0,0 +1,15 @@
+'''
+Condicional if
+'''
+
+dato = int(input("Ingrese un numero: "))
+
+if dato > 0:
+    print("Numero positivo")
+    print("segundo resultado positivo")
+elif dato == 0:
+    print("Resultado 0")
+else:
+    print("Es negativo")
+
+print("Fin")
